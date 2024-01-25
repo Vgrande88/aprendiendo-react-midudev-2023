cae2b809c7d2973643df300990c9f526175cdf73
Victor <vgrande88@gmail.com>
2024-01-25 20:39:59 +0100

    Detected winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -18,17 +18,53 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6], 
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+] 
 
 function App() {
   const [board, setBoard ] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
-  
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinaciones 
+    // ganadoras para saber si hay ganador
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+
+  }
+
   const updateBoard = (index) => {
+    //Si ya tiene algo, no se actualiza posición
+    if (board[index] || winner) return 
+  
     const newBoard = [...board]
     newBoard[index] = turn 
     setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      alert(`El Ganador es: ${newWinner}`)
+      setWinner(newWinner)
+    }
   }
   
   return (
