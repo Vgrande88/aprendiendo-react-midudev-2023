586d757dfc94f572219f0a5a643316e67640c9ff
Victor <vgrande88@gmail.com>
2024-02-13 20:24:39 +0100

    Added new project and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'standard',
+    'standard-jsx'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <h3></h3>
+  )
+}
+
+export default App
